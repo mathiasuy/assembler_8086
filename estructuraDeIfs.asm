0f5f64502c26eb7e2ee74f7e9221cc92c8576365
Mathias Battistella <mathias.battistella@innovant.us>
2022-10-31 04:12:17 -0300

    Manejo de errores

--- estructuraDeIfs.asm
+++ estructuraDeIfs.asm
@@ -27,17 +27,35 @@ OP_OR equ 17;
 OP_DESP_IZQ equ 18;
 OP_DESP_DER equ 19;
 
+PILA_LLENA equ 30 ; hay que considerar al 0
 ADDRESS_STACK equ 0x02000
 ENTRADA equ 1
 PUERTO_SALIDA_DEFECTO equ 0x0010
 PUERTO_LOG_DEFECTO equ 0x0011
 
 .code
-mov si, -2; si quedara reservado para el 'indice' en la pila
+mov si, -2; si quedara reservado para el 'indice' en la pila, empieza en -2 para que la primera inserci?n quede en el lugar 0
 ; ax sera el operador 1
 ; bx sera el operador 1
 mov dx, PUERTO_LOG_DEFECTO; dx quedara reservado globalmente para la bitacora
 mov cx, PUERTO_SALIDA_DEFECTO; cx quedara reservado globalmente para la salida
+; para simplificar las cosas, la inteci?n es que el manejo de la pila y la entrada
+; quede enmascarado en funciones y que no afecten el estado de los registros
+; (o sea si los precisa, que los use, pero que al terminar deje los registros que use de manera auxiliar
+; en el estado anterior al ejecutar la funci?n)
+; se reservar? la menor cantidad posible de registros para tener m?s libertad de uso en el resto del c?digo
+; en particular ax, ya que es el que usan las operaciones mul, idiv, in y out lo usaremos para el comando y el operando 1
+; bx lo usaremos para el operando 2, se usara solo si es realmente necesario. Tanto bx, como ax ser?n manejados por las funciones que 
+; manejan la pila y la entrada. A veces puede evitarse usar 2 registos recurriendo a la pila del sitema (por ejemplo en el swap)
+; dx es el registro que sirve para el indicar el n?mero de puerto que se usar? para la instrucci?n in, 
+; reservaremos este para la bitacora ya que es la que m?s imprime en la salida y adem?s
+; reservaremos cx para guardar el n?mero de puerto de resultados de operaciones y cuando tengamos que imprimir en salida
+; un resultado, asignaremos a dx cx de manera temporal volviendo a dejar dx con el valor anterior luego de imprimir en el puerto
+; Se intentar? maximizar el uso de las funciones definidas, y minimizar el uso directo de instrucciones en el main 
+; pasando siempre por funciones, para que estas puedan centraliar operaciones sobre los registros y controlar aisladamente 
+; el estado de los registros cuando los necesita y/o modifica. Adem?s permitir? que sea  m?s amigable la lectura tanto del main
+; como de las mismas funciones, que teniendo pocas instrucciones y un nombre descriptivo, evidenciar?n 
+; cual es la funcionalidad que cumplen
 
 call poner_siguiente_comando_en_ax
 JZ salir
@@ -88,6 +106,7 @@ while_no_salir:
 		JNZ si_es_NEG
 			call copiar_tope_a_ax
 			call copiar_el_segundo_de_la_pila_a_bx_y_retornar_true_si_no_se_pudo
+			jz fin_si
 			call desapilar
 			call desapilar
 			call apilar_ax
@@ -142,6 +161,7 @@ while_no_salir:
 		cmp ax, OP_SUM
 		JNZ si_es_OP_RES
 			call pre_procesamiento_operacion_binaria
+			jz fin_si
 			add ax, bx 
 			call post_procesamiento_operacion_binaria
 		JMP fin_si
@@ -149,6 +169,7 @@ while_no_salir:
 		cmp ax, OP_RES
 		JNZ si_es_OP_PRO
 			call pre_procesamiento_operacion_binaria
+			jz fin_si
 			neg bx
 			add ax, bx 
 			call post_procesamiento_operacion_binaria
@@ -157,6 +178,7 @@ while_no_salir:
 		cmp ax, OP_PROD
 		JNZ si_es_OP_DIV
 			call pre_procesamiento_operacion_binaria
+			jz fin_si
 			push dx
 			mul bx 
 			pop dx
@@ -167,6 +189,7 @@ while_no_salir:
 		cmp ax, OP_DIV
 		JNZ si_es_OP_MOD
 			call pre_procesamiento_operacion_binaria
+			jz fin_si
 			call swap_ax_con_bx
 			push dx
 			mov dx, 0
@@ -182,6 +205,7 @@ while_no_salir:
 		cmp ax, OP_MOD
 		JNZ si_es_OP_AND
 			call pre_procesamiento_operacion_binaria
+			jz fin_si
 			push dx
 			mov dx, 0
 			cmp ax, 0
@@ -197,6 +221,7 @@ while_no_salir:
 		cmp ax, OP_AND
 		JNZ si_es_OP_OR
 			call pre_procesamiento_operacion_binaria
+			jz fin_si
 			and ax, bx
 			call post_procesamiento_operacion_binaria
 		JMP fin_si
@@ -204,6 +229,7 @@ while_no_salir:
 		cmp ax, OP_OR
 		JNZ si_es_OP_DIZ
 			call pre_procesamiento_operacion_binaria
+			jz fin_si
 			or ax, bx
 			call post_procesamiento_operacion_binaria
 		JMP fin_si
@@ -211,6 +237,7 @@ while_no_salir:
 		cmp ax, OP_DESP_IZQ
 		JNZ si_es_OP_DDE
 			call pre_procesamiento_operacion_binaria
+			jz fin_si
 			or ax, bx
 			call post_procesamiento_operacion_binaria
 		JMP fin_si
@@ -218,18 +245,18 @@ while_no_salir:
 		cmp ax, OP_DESP_DER
 		JNZ si_no
 			call pre_procesamiento_operacion_binaria
+			jz fin_si
 			or ax, bx
 			call post_procesamiento_operacion_binaria
 		JMP fin_si
 		si_no:
+			call log_comando_invalido
 	fin_si:
 	call poner_siguiente_comando_en_ax
 	JMP while_no_salir
 salir:
 
-	mov bx, 2
-	cmp bx, 2
-	jz _saltear
+	jmp _saltear
 
 ;///////// FUNCIONES PARA IMPRESION EN PUERTOS ///////
 
@@ -272,11 +299,21 @@ salir:
 		push ax
 		mov ax, 8
 		out dx, ax
+		mov si, 0 ; vaciar pila
 		cmp ax, 8 ; activo prox JNZ si hay error
 		pop ax
 		ret
 	falta_operando endp
 
+	desbordamiento_de_pila proc
+		push ax
+		mov ax, 4
+		out dx, ax
+		mov si, 0 ; vaciar pila
+		cmp ax, 4 ; activo prox JNZ si hay error
+		pop ax
+		ret
+	desbordamiento_de_pila endp
 
 ;///////// FUNCIONES PARA ENTRADA ////////////////
 	poner_siguiente_operando_en_ax proc
@@ -292,23 +329,43 @@ salir:
 
 ;///////// FUNCIONES PARA PILA ////////////////
 
+	check_pila_llena proc
+		push dx
+		push ax
+		push bx
+		mov ax, si
+		mov dx, 0
+		mov bx, 2
+		idiv bx
+		cmp ax, PILA_LLENA
+		pop bx
+		pop ax
+		pop dx
+		ret
+	check_pila_llena endp
+
 	apilar_ax proc
-		inc si 
-		inc si
+		call check_pila_llena
+		JNZ apilar_ax_continuar
+			call desbordamiento_de_pila
+		apilar_ax_continuar:
+		add si, 2
 		mov word ptr [ADDRESS_STACK+si], ax
 		ret
 	apilar_ax endp
 
 	apilar_bx proc
-		inc si 
-		inc si
+		call check_pila_llena
+		JNZ apilar_ax_continuar
+			call desbordamiento_de_pila
+		apilar_bx_continuar:
+		add si, 2
 		mov word ptr [ADDRESS_STACK+si], bx
 		ret
 	apilar_bx endp
 
 	desapilar proc
-		dec si
-		dec si
+		add si, -2
 		ret
 	desapilar endp
 
@@ -323,13 +380,11 @@ salir:
 	copiar_tope_a_ax endp
 
 	copiar_el_segundo_de_la_pila_a_bx_y_retornar_true_si_no_se_pudo proc; Si no hay segundo operando retorna false
-		dec si
-		dec si
+		add si, -2
 		call pila_no_esta_vacia ; Z=1 si esta vacia
 		JZ terminar_metodo_ ; return true en el c++
 		mov	bx, word ptr [ADDRESS_STACK+si]
-		inc si
-		inc si
+		add si, 2
 		JMP fin_metodo_
 		terminar_metodo_:
 			call falta_operando
@@ -368,16 +423,16 @@ salir:
 
 
 _saltear:
-	mov dx, PUERTO_SALIDA_DEFECTO; asigno a dx el puerto por defecto
-	mov cx, PUERTO_LOG_DEFECTO; asigno a dx el puerto por defecto
 	
 .ports
-ENTRADA: 2, 15, 1, 3, 1, 2, 15, 4, 255
+ENTRADA: 1, 14, 1, 14, 1, 14, 1, 14, 1, 14, 1, 14, 1, 14, 1, 14, 1, 14, 1, 14, 1, 14, 1, 14, 1, 14, 1, 14, 1, 14, 1, 14, 1, 14, 1, 14, 1, 14, 1, 14, 1, 14, 1, 14, 1, 14, 1, 14, 1, 14, 1, 14, 1, 14, 1, 14, 1, 14, 1, 14, 1, 14, 1, 14, 1, 14,  255
 PUERTO_LOG_DEFECTO: 1
 PUERTO_SALIDA_DEFECTO: 1
 
 ;//////////// TEST //////////////////
 
+; test mostrar errores en bitacora comando invalido y falta operando
+; ENTRADA: 200, 1, 14, 11, 14, 1, 1, 11, 4, 255
 ;MOD 
 ;ENTRADA: 2, 15, 1, 3, 1, 2, 15, 4, 255
 
